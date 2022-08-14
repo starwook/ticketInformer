6253ba07b37f67618fb509959b076d7c652d230e
starwook <star.wookk@gmail.com>
2022-08-14 19:42:04 +0900

    boilerPlate

---
+++ src/main/java/com/example/ticketInformer/TicketInformerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketInformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketInformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketInformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketInformer/TicketInformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketInformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketInformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
